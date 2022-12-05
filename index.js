3e38c9479ca592320cc60c675ab6f779bf777e69
Gregory <elvismabisi@gmail.com>
2022-12-05 10:39:13 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = { 
+    name: "Greg",
+    streetAddress: "Ngummo",
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+    }
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const deletedEmployee = {...obj}
+    delete deletedEmployee[key]
+    return deletedEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key]
+    return obj
+}
